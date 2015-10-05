00e79a3cf0688684c693e482177175b64af10ebd
RossTaggart <rosstaggart12@gmail.com>
2015-10-05 16:44:43 +0100

    Work on about dialogue
    still not finished

--- MondaysChild/app/src/main/java/com/example/administrator/mondayschild/mcAboutDialogue.java
+++ MondaysChild/app/src/main/java/com/example/administrator/mondayschild/mcAboutDialogue.java
@@ -1,7 +1,34 @@
 package com.example.administrator.mondayschild;
 
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.os.Bundle;
+
 /**
  * Created by Ross on 05/10/2015.
  */
-public class mcAboutDialogue {
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+
+public class mcAboutDialogue extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+
+        AlertDialog.Builder mcAboutDialog = new AlertDialog.Builder(getActivity());
+        mcAboutDialog.setMessage("This app will take your birthday and tell you what day...")
+                .setPositiveButton("Ok", new DialogeInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialoge, int id){
+
+                    }
+                });
+        mcAboutDialog.setTitle("About");
+        mcAboutDialog.setIcon(R.drawable.ic_menu_action_about);
+
+        return mcAboutDialog.create();
+    }
+
 }
